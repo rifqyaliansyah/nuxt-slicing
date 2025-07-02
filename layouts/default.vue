685a7e806bf219f078fdaaaa168f0dce3db1bbb7
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2025-07-02 14:41:00 +0700

    Update

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,8 +7,8 @@
 </template>
 
 <script>
-import Navbar from '~/components/Navbar.vue'
-import Footer from '~/components/Footer.vue'
+import Navbar from '~/components/layout/Navbar.vue'
+import Footer from '~/components/layout/Footer.vue'
 
 export default {
   components: {
