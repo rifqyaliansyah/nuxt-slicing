b6aaab31b7453630c86513aa82d3eda16f69572d
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2025-07-02 15:03:10 +0700

    Updated

---
+++ components/sections/CourseList.vue
@@ -0,0 +1,18 @@
+<template>
+            <div class="container mx-auto px-6">
+            <div class="flex flex-col md:flex-row items-center justify-between mb-6">
+                <h2 class="text-2xl font-semibold text-gray-800">All <span class="text-green-600">Courses</span> of Edule</h2>
+                <div class="mt-4 md:mt-0">
+                    <input type="text" placeholder="Search course" class="border border-gray-300 px-4 py-2 rounded-md">
+                </div>
+            </div>
+            <div class="flex flex-wrap gap-4 mb-8 justify-center">
+                <a href="#" class="text-gray-600 border border-gray-300 hover:text-green-600 hover:border-green-600 visited:text-green-600 visited:border-green-600 font-semibold px-4 py-2 rounded-md">All</a>
+                <a href="#" class="text-gray-600 border border-gray-300 hover:text-green-600 hover:border-green-600 font-semibold px-4 py-2 rounded-md">Design</a>
+                <a href="#" class="text-gray-600 border border-gray-300 hover:text-green-600 hover:border-green-600 font-semibold px-4 py-2 rounded-md">Development</a>
+                <a href="#" class="text-gray-600 border border-gray-300 hover:text-green-600 hover:border-green-600 font-semibold px-4 py-2 rounded-md">Marketing</a>
+                <a href="#" class="text-gray-600 border border-gray-300 hover:text-green-600 
+                hover:border-green-600 font-semibold px-4 py-2 rounded-md">Business</a>
+            </div>
+        </div>
+</template>
---
+++ components/sections/HeroSection.vue
@@ -0,0 +1,17 @@
+<template>
+        <section class="bg-gray-100 shadow sm py-20">
+        <div class="container mx-auto flex flex-col md:flex-row items-center px-28">
+            <div class="w-full md:w-1/2 space-y-6">
+                <p class="text-sm text-green-600 font-semibold">Start your favorite course</p>
+                <h1 class="text 4xl md:text-5xl font-bold text-gray-700 leading-tight">Now learning from <br> anywhere, and build <br> your <span class="text-green-600">bright career.</span></h1>
+                <p class="text-gray-600">
+                    It has survived not only five centuries, but also <br> the leap into electronic typesetting.
+                </p>
+                <button class="bg-green-600 hover:bg-green-700 text-white font-semibold px-6 py-3 rounded-md">Start A Course</button>
+            </div>
+            <div class="w-full md:w-1/2 md:mt-0 flex justify-center relative">
+                <img src="https://static.vecteezy.com/system/resources/thumbnails/054/040/037/small_2x/a-young-person-in-glasses-writes-in-a-book-wearing-an-orange-sweater-focused-on-their-studies-png.png" alt="hero" class="w-3/4 max-w-xl">
+            </div>
+        </div>
+    </section>
+</template>
