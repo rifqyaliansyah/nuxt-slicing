72e50bc7d9db3620d2580097a8bbdcd0feec146c
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2025-07-03 13:13:53 +0700

    Change

---
+++ pages/course.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="bg-gray-100 py-12 shadow sm">
+            <h1 class="text-3xl text-gray-800 font-bold text-center mt-6">
+            Browse All Courses
+        </h1>
+        <p class="text 2xl text-gray-600 text-center mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        </div>
+        <CourseList />
+        <CourseCard />
+    </div>
+</template>
+
+<script>
+import CourseList from '~/components/sections/CourseList.vue'
+import CourseCard from '~/components/sections/CourseCard.vue'
+
+export default {
+    layout: 'default',
+    components: {
+        CourseList,
+        CourseCard
+    },
+}
+</script>
+
+<style>
+</style>
