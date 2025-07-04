283b78c9f2a442b136347e25f793c2c11740f881
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2025-07-04 10:07:01 +0700

    Update

--- components/sections/CourseList.vue
+++ components/sections/CourseList.vue
@@ -2,8 +2,11 @@
             <div class="container mx-auto px-12 mt-12">
             <div class="flex flex-col md:flex-row items-center justify-between mb-6">
                 <h2 class="text-2xl font-semibold text-gray-800">All <span class="text-green-600">Courses</span> of Edule</h2>
-                <div class="mt-4 md:mt-0">
-                    <input type="text" placeholder="Search course" class="border border-gray-300 px-4 py-2 rounded-md">
+                <div class="mt-4 md:mt-0 relative">
+                    <input type="text" placeholder="Search course" class="border border-gray-300 pl-10 pr-4 py-2 rounded-md w-full">
+                    <svg class="w-5 h-5 text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/2 pointer-events-none" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 1116.65 5a7.5 7.5 0 010 11.65z" />
+                    </svg>
                 </div>
             </div>
             <div class="flex flex-wrap gap-4 mb-8 justify-center">
